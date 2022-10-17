34cd0368ed47e32631db1b0323bb8c1827f413ae
Everett Bazzocchi <contact@everettbazzocchi.ca>
2022-10-17 02:11:27 -0500

    API FIX #5

--- src/Components/Staff/Staff.js
+++ src/Components/Staff/Staff.js
@@ -6,32 +6,32 @@ import SideBar from "./StaffComponents/SideBar";
 import StaffPage from "./StaffComponents/StaffPage";
 
 const Staff = () => {
-  const [staff, setStaff] = useState([]);
-  useEffect(() => {
-    Axios.get("https://api.darklordbazz.com/api/shottyapi/getstaff").then(
-      (response) => {
-        setStaff(response.data.sort((a, b) => (a.id > b.id ? 1 : -1)));
-      }
+    const [staff, setStaff] = useState([]);
+    useEffect(() => {
+        Axios.get("https://api.darklordbazz.com/api/shottyapi/getstaff").then(
+            (response) => {
+                setStaff(response.data.sort((a, b) => (a.id > b.id ? 1 : -1)));
+            }
+        );
+    }, []);
+    return (
+        <div className="staffPage">
+            <SideBar staff={staff} />
+            <div className="profile">
+                <Routes>
+                    {staff.map((staff) => {
+                        var link = `/${staff.username}`;
+                        return (
+                            <Route key={staff.username}
+                                path={link}
+                                element={<StaffPage username={staff.username} />}
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </div>
     );
-  }, []);
-  return (
-    <div className="staffPage">
-      <SideBar staff={staff} />
-      <div className="profile">
-        <Routes>
-          {staff.map((staff) => {
-            var link = `/${staff.username}`;
-            return (
-              <Route key={staff.username}
-                path={link}
-                element={<StaffPage username={staff.username} />}
-              />
-            );
-          })}
-        </Routes>
-      </div>
-    </div>
-  );
 };
 
 export default Staff;
